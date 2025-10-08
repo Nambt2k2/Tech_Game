dedfbdab1dde0d376b061cbc773c3e09c5deca8c
PointB <nambt2k2@gmail.com>
2025-10-09 01:38:05 +0700

    Thêm logic data cố định cho nguyên liệu và công trình

---
+++ Assets/_GAME/Scripts/MaterialDataConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "MaterialDataConfig", menuName = "DATA/Material Data Config")]
+public class MaterialDataConfig : ScriptableObject {
+    public E_NameMaterial e_name;
+    public Sprite sprite;
+}
+
+public enum E_NameMaterial {
+    GỖ, ĐÁ, PEG, SỢI, LEN, VẢI, THỎI, DA, ĐINH, HẠT_MÂM_XÔI, NGUYÊN_TỐ_LỬA, NGUYÊN_TỐ_NƯỚC, NGUYÊN_TỐ_BĂNG,
+}
---
+++ Assets/_GAME/Scripts/TechDataConfig.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "TechDataConfig", menuName = "DATA/Tech Data Config")]
+public class TechDataConfig : ScriptableObject {
+    public E_NameTech e_name;
+    public int levelTech;
+    public S_Material[] arr_material;
+    public S_TechStat stat;
+    public string description;
+    public int workload;
+    public E_TypeAttributeTech[] arr_typeAttribute;
+    public Sprite sprite;
+}
+
+[System.Serializable]
+public struct S_Material {
+    public E_NameMaterial e_name;
+    public int amount;
+}
+
+[System.Serializable]
+public struct S_TechStat {
+    public int attack;
+    public int durability;
+    public int wear;
+    public int weight;
+}
+
+
+public enum E_NameTech {
+
+}
+
+public enum E_TypeAttributeTech {
+    THỦ_CÔNG, TRỒNG_TRỌT, TƯỚI_NƯỚC, VẬN_CHUYỂN, TRANG_TRẠI, ĐỐN_GỖ, KHAI_KHOÁNG, ĐỐT_LỬA,
+}
