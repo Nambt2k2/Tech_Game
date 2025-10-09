7759f59252106b1052ce38507bc734b7fae07b41
Nam <nambt2k2@gmail.com>
2025-10-10 00:49:25 +0700

    sửa cấu trúc data cố định, thêm data cố định cho công trình

--- Assets/_GAME/Scripts/MaterialDataConfig.cs
+++ Assets/_GAME/Scripts/MaterialDataConfig.cs
@@ -2,10 +2,11 @@
 
 [CreateAssetMenu(fileName = "MaterialDataConfig", menuName = "DATA/Material Data Config")]
 public class MaterialDataConfig : ScriptableObject {
-    public E_NameMaterial e_name;
+    public E_IDMaterial id;
+    public string nameMaterial;
     public Sprite sprite;
 }
 
-public enum E_NameMaterial {
+public enum E_IDMaterial {
     GỖ, ĐÁ, PEG, SỢI, LEN, VẢI, THỎI, DA, ĐINH, HẠT_MÂM_XÔI, NGUYÊN_TỐ_LỬA, NGUYÊN_TỐ_NƯỚC, NGUYÊN_TỐ_BĂNG,
 }
